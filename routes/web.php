09871c1aed3c0daa6eab32a434b5d2d43c31cfd8
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-21 00:22:39 +0100

    done!

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/just-for-fun', function () {
+    return view('just-for-fun');
+});
