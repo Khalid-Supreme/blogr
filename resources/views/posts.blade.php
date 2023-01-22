99e42a2dd9698fbc27790eb432c1d14e0fe15009
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-22 19:10:26 +0100

    done 2

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,15 +6,15 @@
 
 <body>
     <article>
-        <h1><a href="/post"> My First Post</a></h1>
+        <h1><a href="/post/my-first-post"> My First Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione corrupti ab tenetur? Eius molestias, reiciendis sed officiis cum mollitia facilis culpa voluptatum. Delectus, modi accusamus cum doloremque alias ex consequuntur iste soluta! Illum, ipsa laborum fugiat ipsum inventore vero sunt voluptas soluta sapiente repudiandae porro ipsam recusandae aliquid obcaecati optio voluptate, dolorum ad? Incidunt assumenda alias necessitatibus aut eaque distinctio ea ipsa et facilis nesciunt, in reprehenderit voluptatum deserunt eum ratione molestiae deleniti inventore debitis sapiente modi amet aperiam? Corrupti inventore provident illo dolore at consequatur illum blanditiis ipsa ipsam, deserunt dolorem quam quidem eligendi optio amet eius harum?</p>
     </article>
     <article>
-        <h1><a href="/post"> My Second Post</a></h1>
+        <h1><a href="/post/my-second-post"> My Second Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione corrupti ab tenetur? Eius molestias, reiciendis sed officiis cum mollitia facilis culpa voluptatum. Delectus, modi accusamus cum doloremque alias ex consequuntur iste soluta! Illum, ipsa laborum fugiat ipsum inventore vero sunt voluptas soluta sapiente repudiandae porro ipsam recusandae aliquid obcaecati optio voluptate, dolorum ad? Incidunt assumenda alias necessitatibus aut eaque distinctio ea ipsa et facilis nesciunt, in reprehenderit voluptatum deserunt eum ratione molestiae deleniti inventore debitis sapiente modi amet aperiam? Corrupti inventore provident illo dolore at consequatur illum blanditiis ipsa ipsam, deserunt dolorem quam quidem eligendi optio amet eius harum?</p>
     </article>
     <article>
-        <h1><a href="/post"> My Third Post</a></h1>
+        <h1><a href="/post/my-third-post"> My Third Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione corrupti ab tenetur? Eius molestias, reiciendis sed officiis cum mollitia facilis culpa voluptatum. Delectus, modi accusamus cum doloremque alias ex consequuntur iste soluta! Illum, ipsa laborum fugiat ipsum inventore vero sunt voluptas soluta sapiente repudiandae porro ipsam recusandae aliquid obcaecati optio voluptate, dolorum ad? Incidunt assumenda alias necessitatibus aut eaque distinctio ea ipsa et facilis nesciunt, in reprehenderit voluptatum deserunt eum ratione molestiae deleniti inventore debitis sapiente modi amet aperiam? Corrupti inventore provident illo dolore at consequatur illum blanditiis ipsa ipsam, deserunt dolorem quam quidem eligendi optio amet eius harum?</p>
     </article>
 
