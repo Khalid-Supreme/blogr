79c0f78651d8773aaf8d68b4141fdd8dba56a056
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-22 19:27:14 +0100

    done!

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
     return view('posts');
 });
 Route::get('/post/{post}', function ($slug) {
-    
+
     // $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    if(!file_exists($path)){
+    if (!file_exists($path)) {
         // ddd('file does not exist');
         // dd('file does not exist');
         return redirect('/');
@@ -29,6 +29,7 @@ Route::get('/post/{post}', function ($slug) {
     $post = file_get_contents($path);
 
     return view('post', [
-        'post' => ($post)
+        'post' => $post
     ]);
-});
+})->where('post', '[A-z_\-]+');
+// })->whereAlpha('post');
