51c7458e15740e79ee7b36f44d02c49cdae1b6f5
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-22 11:44:24 +0100

    done!

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="style.css">
+
+<title>Blogr-Home</title>
+
+<body>
+    <article>
+        <h1><a href="/post"> My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione corrupti ab tenetur? Eius molestias, reiciendis sed officiis cum mollitia facilis culpa voluptatum. Delectus, modi accusamus cum doloremque alias ex consequuntur iste soluta! Illum, ipsa laborum fugiat ipsum inventore vero sunt voluptas soluta sapiente repudiandae porro ipsam recusandae aliquid obcaecati optio voluptate, dolorum ad? Incidunt assumenda alias necessitatibus aut eaque distinctio ea ipsa et facilis nesciunt, in reprehenderit voluptatum deserunt eum ratione molestiae deleniti inventore debitis sapiente modi amet aperiam? Corrupti inventore provident illo dolore at consequatur illum blanditiis ipsa ipsam, deserunt dolorem quam quidem eligendi optio amet eius harum?</p>
+    </article>
+    <article>
+        <h1><a href="/post"> My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione corrupti ab tenetur? Eius molestias, reiciendis sed officiis cum mollitia facilis culpa voluptatum. Delectus, modi accusamus cum doloremque alias ex consequuntur iste soluta! Illum, ipsa laborum fugiat ipsum inventore vero sunt voluptas soluta sapiente repudiandae porro ipsam recusandae aliquid obcaecati optio voluptate, dolorum ad? Incidunt assumenda alias necessitatibus aut eaque distinctio ea ipsa et facilis nesciunt, in reprehenderit voluptatum deserunt eum ratione molestiae deleniti inventore debitis sapiente modi amet aperiam? Corrupti inventore provident illo dolore at consequatur illum blanditiis ipsa ipsam, deserunt dolorem quam quidem eligendi optio amet eius harum?</p>
+    </article>
+    <article>
+        <h1><a href="/post"> My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ratione corrupti ab tenetur? Eius molestias, reiciendis sed officiis cum mollitia facilis culpa voluptatum. Delectus, modi accusamus cum doloremque alias ex consequuntur iste soluta! Illum, ipsa laborum fugiat ipsum inventore vero sunt voluptas soluta sapiente repudiandae porro ipsam recusandae aliquid obcaecati optio voluptate, dolorum ad? Incidunt assumenda alias necessitatibus aut eaque distinctio ea ipsa et facilis nesciunt, in reprehenderit voluptatum deserunt eum ratione molestiae deleniti inventore debitis sapiente modi amet aperiam? Corrupti inventore provident illo dolore at consequatur illum blanditiis ipsa ipsam, deserunt dolorem quam quidem eligendi optio amet eius harum?</p>
+    </article>
+
+</body>
