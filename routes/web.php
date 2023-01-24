0a6ad2c9450fb309fb8dc58fbfa6204a666876d9
Khalid Supreme <richunclekhalid@gmail.com>
2023-01-24 22:45:55 +0100

    done again

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,16 @@ Route::get('/post/{post}', function ($slug) {
         return redirect('/');
         // abort(404);
     }
-    $post = file_get_contents($path);
+    // $post =  file_get_contents($path);
+    $post = cache()->remember(
+        "posts.{$slug}",
+        5,
+        function () use ($path) {
+            var_dump($path);
+            return file_get_contents($path);
+
+        }
+    );
 
     return view('post', [
         'post' => $post
